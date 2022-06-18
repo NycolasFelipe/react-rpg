82c1cb2c8a4faec69df6ab8b95a282a3ec95556c
Nycolas Felipe Lourenço <nycolasfelipe.contato@gmail.com>
2022-06-18 15:51:24 -0300

    Updated Links

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -9,37 +9,43 @@ export default function Footer(props) {
       <h1 className='footer-title'>Thank you for visiting!</h1>
       <div className='footer-links'>
         <div className='link-github'>
-          <img 
-            src="/images/github.png" 
-            className=
-            {
-              'link-icon ' + (darkMode ? 'link-icon--lm' : '')
-            }
-            alt="GitHub Link"
-          />
-          <p>GitHub</p>
+          <a target="_blank" href="https://github.com/NycolasFelipe">
+            <img 
+              src="/images/github.png" 
+              className=
+              {
+                'link-icon ' + (darkMode ? 'link-icon--lm' : '')
+              }
+              alt="GitHub Link"
+            />
+            <p>GitHub</p>
+          </a>
         </div>
         <div className='link-linkedin'>
-          <img 
-            src="/images/linkedin.png" 
-            className=
-            {
-              'link-icon ' + (darkMode ? 'link-icon--lm' : '')
-            }
-            alt="LinkedIn Link"
-          />
-          <p>LinkedIn</p>
+          <a target="_blank" href="https://www.linkedin.com/in/nycolas-felipe-louren%C3%A7o-0448b6150/">
+            <img 
+              src="/images/linkedin.png" 
+              className=
+              {
+                'link-icon ' + (darkMode ? 'link-icon--lm' : '')
+              }
+              alt="LinkedIn Link"
+            />
+            <p>LinkedIn</p>
+          </a>
         </div>
         <div className='link-email'>
-          <img 
-            src="/images/email.png" 
-            className=
-            {
-              'link-icon ' + (darkMode ? 'link-icon--lm' : '')
-            }
-            alt="Email Link"
-          />
-          <p>Send an email</p>
+          <a target="_blank" href="https://mail.google.com/mail/?view=cm&fs=1&to=nycolasfelipe.contato@gmail.com&su=CONTACT">
+            <img 
+              src="/images/email.png" 
+              className=
+              {
+                'link-icon ' + (darkMode ? 'link-icon--lm' : '')
+              }
+              alt="Email Link"
+            />
+            <p>Send an email</p>
+          </a>
         </div>
       </div>
       <div className='footer-copyright'>
